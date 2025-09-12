4dfdaeabfdb69940a8b73fb3943038dbca98a66c
DigitallLearning <digitallearning1505@gmail.com>
2025-09-12 12:26:55 +0530

    first commit

---
+++ src/MyClassCmp.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+class MyClassCmp extends Component
+{
+    render()
+    {
+        return(
+          <h1>Class Component</h1>
+        )
+    }
+}
+export default MyClassCmp
---
+++ src/MyFunCmp.js
@@ -0,0 +1,7 @@
+function MyFunCmp()
+{
+    return(
+        <h1>This is Function Component</h1> //JSX
+    )
+}
+export default MyFunCmp
