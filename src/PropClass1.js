19567213c2dd659daa6264d0c99223f910c7e962
DigitallLearning <digitallearning1505@gmail.com>
2025-09-13 12:35:34 +0530

    prop1

--- src/PropClass1.js
+++ src/PropClass1.js
@@ -1,13 +1,22 @@
 import { Component } from "react";
 import PropClass2 from "./PropClass2";
-
 class PropClass1 extends Component
 {
+    constructor()
+    {
+        super();
+        this.state=
+        {
+            uname:"Ram",
+            uage:25
+        }
+    }
    render()
    {
     return(
         <div>
-           <PropClass2 name="Ram" age="25"></PropClass2>
+           <PropClass2 name={this.state.uname} age={this.state.uage}></PropClass2>
+           <button onClick={()=>this.setState({uname:"Syam",uage:30})}>Submit</button>
         </div>
     )
    }
